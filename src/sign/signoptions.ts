3ddb89535492b735e0c2f4e6fc0955d9caa2ba98
Daniel Breen <daniel.breen@royaljay.local>
2019-05-19 17:23:42 -0600

    Add 'auto' sign option

--- src/sign/signoptions.ts
+++ src/sign/signoptions.ts
@@ -1,8 +1,10 @@
 export interface ISignOptions {
     rawString?: string;
+    auto?: boolean;
     rfcTimeStampUrl?: string;
 }
 
 export enum SignOptionsFlag {
+    auto = '/a',
     rfcTimeStampUrl = '/tr'
 }
